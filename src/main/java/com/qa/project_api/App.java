f68a7b8af8e8b4324846485f9069540d3127cdb9
Joe Gurney <jgurnney@gmail.com>
2019-01-22 14:21:32 +0100

    init

---
+++ src/main/java/com/qa/project_api/App.java
@@ -0,0 +1,9 @@
+package com.qa.project_api;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //should be empty
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO film (id, title, genre, ageRating) VALUES (1, 'Jurrasic Park', 'Adventure', '12');
+INSERT INTO film (id, title, genre, ageRating) VALUES (2, 'Predator', 'Thriller', '15');
+INSERT INTO film (id, title, genre, ageRating) VALUES (3, 'Friday the 13th', 'Horror', '18');
