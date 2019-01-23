79f93bd13958aae56ced04c78d41a2ad0d33dd3a
Joe Gurney <jgurnney@gmail.com>
2019-01-23 08:52:59 +0100

    added more to import sql and fixed error in DBrepo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,4 @@
 INSERT INTO recipe (recipeId, title, readyTime, servings) VALUES (1, 'first', '50', '6');
 INSERT INTO recipe (recipeId, title, readyTime, servings) VALUES (2, 'second', '40', '4');
 INSERT INTO recipe (recipeId, title, readyTime, servings) VALUES (3, 'third', '30', '10');
+INSERT INTO recipe (recipeId, title, readyTime, servings, ingredients, method) VALUES (4, 'third', '30', '10', 'bla bla bla', 'something else');
