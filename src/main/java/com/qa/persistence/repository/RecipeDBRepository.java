855361fd09eaa932b6747a3a1b15f191879e6921
Joe Gurney <jgurnney@gmail.com>
2019-02-08 08:51:03 +0100

    error handling

--- src/main/java/com/qa/persistence/repository/RecipeDBRepository.java
+++ src/main/java/com/qa/persistence/repository/RecipeDBRepository.java
@@ -5,11 +5,7 @@ import javax.inject.Inject;
 import javax.persistence.Query;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceException;
 import javax.transaction.Transactional;
-
-import org.hibernate.JDBCException;
-import org.hibernate.exception.ConstraintViolationException;
 
 import com.qa.persistence.domain.Recipe;
 import com.qa.util.JSONUtil;
@@ -22,12 +18,12 @@ import java.util.Collection;
 @Default
 public class RecipeDBRepository implements RecipeRepository {
 
-	@PersistenceContext(unitName="primary")
+	@PersistenceContext(unitName = "primary")
 	private EntityManager manager;
-	
+
 	@Inject
 	private JSONUtil util;
-	
+
 	public String getAllRecipes() {
 		Query query = manager.createQuery("SELECT a FROM Recipe a");
 		@SuppressWarnings("unchecked")
@@ -36,16 +32,20 @@ public class RecipeDBRepository implements RecipeRepository {
 	}
 
 	public String getRecipesByUser(String email) {
-		Query query = manager.createQuery("SELECT a FROM Recipe a WHERE a.user LIKE '"+ email + "'");
-		@SuppressWarnings("unchecked")
-		Collection<Recipe> myRecipes = (Collection<Recipe>) query.getResultList();
-		return util.getJSONForObject(myRecipes);
+		try {
+			Query query = manager.createQuery("SELECT a FROM Recipe a WHERE a.user LIKE '" + email + "'");
+			@SuppressWarnings("unchecked")
+			Collection<Recipe> myRecipes = (Collection<Recipe>) query.getResultList();
+			return util.getJSONForObject(myRecipes);
+		} catch (Exception e) {
+			return "{\"message\": \"No recipes by that user found\"}";
+		}
 	}
-	
+
 	@Transactional(REQUIRED)
 	public String removeRecipeById(int id) {
 		Recipe toRemove = findRecipe(id);
-		if(toRemove != null) {
+		if (toRemove != null) {
 			manager.remove(toRemove);
 			return "{\"message\": \"Recipe removed\"}";
 		}
@@ -63,9 +63,8 @@ public class RecipeDBRepository implements RecipeRepository {
 			Recipe newRecipe = util.getObjectForJSON(recipe, Recipe.class);
 			newRecipe.setUser(email);
 			manager.persist(newRecipe);
-			return "{\"message\": \"Recipe added as "+ email + "\"}";
-		}
-		catch (Exception e) {
+			return "{\"message\": \"Recipe added as " + email + "\"}";
+		} catch (Exception e) {
 			return "{\"message\": \"Failed to add recipe check fields or saved recipes and try again\"}";
 		}
 	}
